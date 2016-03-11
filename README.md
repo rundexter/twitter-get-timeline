1ea03058b07be21a57218465dc539a937c88e639
Mike Friedman <friedo@friedo.com>
2016-03-11 13:49:34 -0500

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dexter Module
+Returns a collection of the most recent Tweets and retweets posted by the specified user.
---
+++ index.js
@@ -0,0 +1,30 @@
+var Twit = require('twit');
+var _ = require('lodash');
+
+module.exports = {
+    /**
+     * Allows the authenticating users to follow the user specified in the ID parameter.
+     *
+     * @param {AppStep} step Accessor for the configuration for the step using this module.  Use step.input('{key}') to retrieve input data.
+     * @param {AppData} dexter Container for all data used in this workflow.
+     */
+    run: function(step, dexter) {
+        var credentials = dexter.provider('twitter').credentials(),
+            twitter = new Twit({
+                access_token: credentials.access_token,
+                access_token_secret: credentials.access_token_secret,
+                consumer_key: credentials.consumer_key,
+                consumer_secret: credentials.consumer_secret
+            });
+
+        twitter.get('statuses/user_timeline', step.inputs(), function (error, listTweets) {
+            if (error) {
+                // if error - send message
+                this.fail(error);
+            } else {
+                // return befriendedInfo
+                this.complete({tweets: listTweets});
+            }
+        }.bind(this));
+    }
+};
